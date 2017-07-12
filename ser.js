fbbcc4c8cb34896d8c101a0aa8cbf92d5549beb8
a712718 <1023940468@qq.com>
2017-07-12 17:41:08 +0800

    contentBase

--- ser.js
+++ ser.js
@@ -20,3 +20,7 @@ new WebpackDevServer(compiler, {
     publicPath: '/assets/'
 })
 .listen(port)
+// 开发环境下，webpack-server会自动到 contentBase 指定的路径下找index.html页面 
+//然后前端请求时默认返回index.html
+// 开发环境下 webpack会把打包后的文件放到内存的/assets/文件夹下，想读取 /assets/里的文件，
+//要设置静态文件目录即publicPath 
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
 
 let env = process.env.NODE_ENV;
-
+console.log(env)
 module.exports = require(path.resolve(__dirname, 'cfg', env));
